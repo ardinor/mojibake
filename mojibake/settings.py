f400af21f3192433a7c77944af87c0d3ac9f705d
ardinor <temnoyume@gmail.com>
2014-06-11 12:11:55 +1000

    Changes to the auth_log_parser to also catch root login attempts

--- mojibake/settings.py
+++ mojibake/settings.py
@@ -35,12 +35,20 @@ else:
     SQLALCHEMY_DATABASE_URI = "mysql:///" + username + ":" + password + "@localhost/mojibake"
     SECRET_KEY = config.get("credentials", "secret_key")
 
-###
+### Settings for auth_log_parser ###
+SERVER_NAME = 'defestri'
 LOG_DIR = '/var/log/'
 
-SEARCH_STRING = '(?P<log_date>^.*) defestri sshd.*Invalid user (?P<user>.*) from (?P<ip_add>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+# Search string for the usual invalid user login attempts from /var/log/auth.log
+# Jun 10 12:40:05 defestri sshd[11019]: Invalid user admin from 61.174.51.217
+SEARCH_STRING = '(?P<log_date>^.*) {server} sshd.*Invalid user (?P<user>.*) from (?P<ip_add>\d{{1,3}}\.\d{{1,3}}\.\d{{1,3}}\.\d{{1,3}})'.format(server=SERVER_NAME)
+# Search string for root login attempts from /var/log/auth.log
+# Jun  8 04:31:10 defestri sshd[5013]: User root from 116.10.191.234 not allowed because none of user's groups are listed in AllowGroups
+ROOT_NOT_ALLOWED_SEARCH_STRING = '(?P<log_date>^.*) {server} sshd.*User (?P<user>.*) from (?P<ip_add>\d{{1,3}}\.\d{{1,3}}\.\d{{1,3}}\.\d{{1,3}}) not allowed'.format(server=SERVER_NAME)
+# Search string for fail2ban banning repeat offenders as logged in /var/log/fail2ban.log
+# 2014-06-10 12:40:07,363 fail2ban.actions: WARNING [ssh] Ban 61.174.51.217
 FAIL2BAN_SEARCH_STRING = '(?P<log_date>^.*) fail2ban.actions: WARNING \[ssh] Ban (?P<ip_add>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
 
 API_URL = 'http://api.ipinfodb.com/v3/ip-city/'
 API_KEY = ''
-###
+### End auth_log_parser settings ###
