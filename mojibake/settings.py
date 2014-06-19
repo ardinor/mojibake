66fe28114f28f4e20aef9f4603f7d4e15577ac52
ardinor <temnoyume@gmail.com>
2014-06-19 15:28:40 +1000

    Move the auth_log_parser out of mojibake - see scrutiny

--- mojibake/settings.py
+++ mojibake/settings.py
@@ -53,21 +53,3 @@ else:
     '[in %(pathname)s:%(lineno)d]'
     ))
     #app.logger.addHandler(file_handler)
-
-### Settings for auth_log_parser ###
-HOST_SERVER_NAME = 'defestri'
-LOG_DIR = '/var/log/'
-
-# Search string for the usual invalid user login attempts from /var/log/auth.log
-# Jun 10 12:40:05 defestri sshd[11019]: Invalid user admin from 61.174.51.217
-SEARCH_STRING = '(?P<log_date>^.*) {server} sshd.*Invalid user (?P<user>.*) from (?P<ip_add>\d{{1,3}}\.\d{{1,3}}\.\d{{1,3}}\.\d{{1,3}})'.format(server=HOST_SERVER_NAME)
-# Search string for root login attempts from /var/log/auth.log
-# Jun  8 04:31:10 defestri sshd[5013]: User root from 116.10.191.234 not allowed because none of user's groups are listed in AllowGroups
-ROOT_NOT_ALLOWED_SEARCH_STRING = '(?P<log_date>^.*) {server} sshd.*User (?P<user>.*) from (?P<ip_add>\d{{1,3}}\.\d{{1,3}}\.\d{{1,3}}\.\d{{1,3}}) not allowed'.format(server=HOST_SERVER_NAME)
-# Search string for fail2ban banning repeat offenders as logged in /var/log/fail2ban.log
-# 2014-06-10 12:40:07,363 fail2ban.actions: WARNING [ssh] Ban 61.174.51.217
-FAIL2BAN_SEARCH_STRING = '(?P<log_date>^.*) fail2ban.actions: WARNING \[ssh] Ban (?P<ip_add>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
-
-API_URL = 'http://api.ipinfodb.com/v3/ip-city/'
-API_KEY = ''
-### End auth_log_parser settings ###
