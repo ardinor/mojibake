55ba2c2310a0f3a4a413801ce8edf52e001c9ffd
ardinor <temnoyume@gmail.com>
2014-09-01 16:05:48 +1000

    Set the process title on posix systems

--- tornado_srv.py
+++ tornado_srv.py
@@ -2,10 +2,16 @@ import tornado.web
 import tornado.wsgi
 import tornado.httpserver
 import tornado.ioloop
+import os
 
 from mojibake.main import app
 from mojibake.settings import PORT
 
+if os.name == 'posix':
+    import setproctitle
+    setproctitle.setproctitle('mojibake')  # Set the process title to mojibake
+
+print('Starting Mojibake...')
 container = tornado.wsgi.WSGIContainer(app)
 http_server = tornado.httpserver.HTTPServer(container)
 http_server.listen(PORT)
