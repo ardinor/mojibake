b4ee7fec8d1b8141cbc54c9c13097ef6cc808b72
ardinor <temnoyume@gmail.com>
2014-09-02 12:15:26 +1000

    For the archive_year function, check if we're in debug or not to perform the right query for the right db

--- mojibake/views/archive.py
+++ mojibake/views/archive.py
@@ -2,6 +2,7 @@ from flask import Blueprint, abort, g, render_template
 from sqlalchemy.sql import func
 
 from mojibake.models import Post
+from mojibake.settings import DEBUG
 
 archive = Blueprint('archive', __name__,
     template_folder='templates')
@@ -20,15 +21,23 @@ def archive_list():
 @archive.route('/<year>/')
 def archive_year(year):
     if g.user is not None and g.user.is_authenticated():
-        #year_posts = Post.query.filter("strftime('%Y', date) = :year"). \
-        #    params(year=year).order_by('-date').all()
-        year_posts = Post.query.filter(func.YEAR(Post.date) == year). \
-            params(year=year).order_by('-date').all()
+        # The dev environment is using sqlite, production MySQL
+        # The way to perform the query is different depending on the database
+        # and as far as I know, there's no database agnostic way of performing
+        # the query.
+        if DEBUG:
+            year_posts = Post.query.filter("strftime('%Y', date) = :year"). \
+                params(year=year).order_by('-date').all()
+        else:
+            year_posts = Post.query.filter(func.YEAR(Post.date) == year). \
+                params(year=year).order_by('-date').all()
     else:
-        #year_posts = Post.query.filter("strftime('%Y', date) = :year"). \
-        #    params(year=year).filter_by(published=True).order_by('-date').all()
-        year_posts = Post.query.filter(func.YEAR(Post.date) == year). \
-            params(year=year).filter_by(published=True).order_by('-date').all()
+        if DEBUG:
+            year_posts = Post.query.filter("strftime('%Y', date) = :year"). \
+                params(year=year).filter_by(published=True).order_by('-date').all()
+        else:
+            year_posts = Post.query.filter(func.YEAR(Post.date) == year). \
+                params(year=year).filter_by(published=True).order_by('-date').all()
 
     if year_posts:
         return render_template('archive_year.html', year=year,
