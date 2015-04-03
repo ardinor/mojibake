0d687ef8527a23b3480d0dad771fb510332895b0
ardinor <jordan@defestri.org>
2015-04-03 14:52:21 +1000

    Handle available_languages a bit better, check locale exists before trying to return available languages, if it doesn't assume we're running in English

--- mojibake/available_languages.py
+++ mojibake/available_languages.py
@@ -4,9 +4,13 @@ from mojibake.settings import LANGUAGES
 
 
 def available_languages(locale):
-    available_languages = [(key, value) for key, value in LANGUAGES.items() if key not in locale]
-    if available_languages[0][1] == '日本語':
+    if locale:
+        available_languages = [(key, value) for key, value in LANGUAGES.items() if key not in locale]
+        if available_languages[0][1] == '日本語':
+            return ('ja', '日本語で')
+        elif available_languages[0][1] == 'English':
+            return ('en', 'In English')
+        return available_languages[0][1]
+    else:
+        # If we couldn't get the session's language, assume we're in English
         return ('ja', '日本語で')
-    elif available_languages[0][1] == 'English':
-        return ('en', 'In English')
-    return available_languages[0][1]
